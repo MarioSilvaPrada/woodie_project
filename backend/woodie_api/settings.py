ada3b5c312e7a85d3dbd5b6ad90e672fb5a927ea
MarioSilvaPrada <mariosilvaprada@gmail.com>
2021-08-05 11:34:17 +0100

    update cors

--- backend/woodie_api/settings.py
+++ backend/woodie_api/settings.py
@@ -35,7 +35,7 @@ ALLOWED_HOSTS = ['woodi-backend.herokuapp.com',
 
 CORS_ORIGIN_WHITELIST = ('http://localhost:9000',
                          'http://localhost:8000',
-                         'https://mystifying-carson-cd65e2.netlify.app')
+                         'https://woodi.shop')
 
 
 if DEBUG == 0:
