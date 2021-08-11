fbe1a54ff5b6a713f3250ba818c6e70762be6a8b
MarioSilvaPrada <mariosilvaprada@gmail.com>
2021-08-11 16:04:26 +0100

    update aws bucket name

--- backend/woodie_api/settings.py
+++ backend/woodie_api/settings.py
@@ -113,7 +113,7 @@ STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
 
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = 'woodie-static-files'
+AWS_STORAGE_BUCKET_NAME = 'woodi-static-files'
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.eu-west-2.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
 AWS_DEFAULT_ACL = 'public-read'
