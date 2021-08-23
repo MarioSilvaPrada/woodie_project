7bff9fc8a65b5c3c482b689305cddd3bbcd2cbcd
MarioSilvaPrada <mariosilvaprada@gmail.com>
2021-08-23 07:31:41 +0100

    added favicon

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -76,7 +76,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: 'src/index.html', //source html
-      favicon: 'src/assets/images/favicon.png',
+      favicon: 'src/assets/images/fav.svg',
     }),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify(
